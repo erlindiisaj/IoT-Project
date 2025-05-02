3e2ba99e7581a77b69ac82bab0c84c3e81116ba6
Erlindi Isaj <52315634+erlindiisaj@users.noreply.github.com>
2025-05-02 12:16:47 +0300

    motor ldr and pir sensor codes

---
+++ motor-ldr/sketch_may1a/sketch_may1a.ino
@@ -0,0 +1,27 @@
+#include <ESP32Servo.h>
+
+Servo myServo;
+const int ldrPin = 35;  // Use GPIO 36 (A0)
+
+void setup() {
+  Serial.begin(115200);
+  myServo.attach(22); // GPIO 2 for servo
+}
+
+void loop() {
+  int lightValue = analogRead(ldrPin);  // Read LDR value (0–4095 on ESP32)
+  Serial.println(lightValue);
+
+  if (lightValue < 1000) {
+    // Dark → rotate clockwise
+    myServo.write(0);
+  } else if (lightValue > 3000) {
+    // Bright → rotate counterclockwise
+    myServo.write(180);
+  } else {
+    // Medium light → stop
+    myServo.write(90);
+  }
+
+  delay(500);
+}
