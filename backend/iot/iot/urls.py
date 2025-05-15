92401f4ed2b868261976f5e7138118e819143502
Leonit Shabani <leonitshabani3@gmail.com>
2025-05-16 02:12:27 +0300

    Add comment to clarify project-level URL routing

--- backend/iot/iot/urls.py
+++ backend/iot/iot/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# you route at project level
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('iotapp.urls')),
