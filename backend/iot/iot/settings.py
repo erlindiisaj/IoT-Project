9a949267b61e63e4f96960effc5c3462eb31edcf
Leonit Shabani <leonitshabani3@gmail.com>
2025-05-18 17:45:02 +0300

    update

--- backend/iot/iot/settings.py
+++ backend/iot/iot/settings.py
@@ -48,7 +48,10 @@ ASGI_APPLICATION = 'iot.asgi.application'
 # settings.py (for development only)
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 5173)],
+        },
     },
 }
 
