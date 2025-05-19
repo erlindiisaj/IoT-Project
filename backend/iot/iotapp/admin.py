4028fe887e69ad9782281882c6e5ece215ae7f13
Leonit Shabani <leonitshabani3@gmail.com>
2025-05-19 06:43:03 +0300

    Cleaning up

--- backend/iot/iotapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Room, Component, ComponentData
-
-# Register your models here.
-admin.site.register(Room)
-admin.site.register(Component)
-admin.site.register(ComponentData)
